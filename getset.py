c6b8f95503a7a60c13dd1a4944c5e848fe373d80
PhilipWang <wh_happy2014@163.com>
2018-05-17 16:31:46 +0800

    python

---
+++ getset.py
@@ -0,0 +1,28 @@
+
+
+class Student(object):
+	"""docstring for Stedent"""
+	def __init__(self,name,score):
+		
+		self.__name = name
+		self.__score = score
+	def info(self):
+		print('学生：%s; 分数:%s'%(self.__name,self.__score))
+
+	def get_score(self):
+		return self.__score
+
+	def set_score(self,score):
+		if 0<=score<=100:
+			self.__score=score
+		else:
+		    print('请输入0到100的数字。')		
+
+#set用来避免传入无效的参数		
+
+stu=Student('xiaoming',95)
+print('修改前分数:',stu.get_score())
+stu.info()
+stu.set_score(122)
+print('修改后分数:',stu.get_score())
+stu.info()
---
+++ weather.py
@@ -0,0 +1,50 @@
+#继承 多态 封装 python类的用法
+
+class WeatherSearch(object):
+	"""docstring for WeatherSearch"""
+	def __init__(self, input_daytime):
+		self.input_daytime = input_daytime
+
+	def seach_visibility(self):
+		visible_level=0
+		if self.input_daytime=='daytime':
+			visible_level=2
+		if self.input_daytime=='night':
+			visible_level=9
+		return visible_level
+
+	def seach_temperature():
+		temperature=0
+		if self.input_daytime=='daytime':
+		    temperature=26
+		if self.input_daytime=='night':
+			temperature=16
+		return temperature
+
+
+class OutAdvice(WeatherSearch):
+	"""docstring for OutAdvice"""
+	def __init__(self, input_daytime):
+		WeatherSearch.__init__(self,input_daytime)
+
+	def seach_temperature(self):
+		vehicle=''
+		if self.input_daytime=='daytime':
+			vehicle='bike'
+		if self.input_daytime=='night':
+			vehicle='taxi'
+		return vehicle
+
+	def out_advice(self):
+		visible_level=self.seach_visibility()
+		if visible_level==2:
+			print('The weather is good,suitable for use %s.'%self.seach_temperature())
+		elif visible_level==9:
+			print('The weather is bad,you should use %s.'%self.seach_temperature())
+		else:
+			print('The weather is beyond my scope,I can not give you any advice')
+
+sth=input('请输入天气情况')
+check=OutAdvice(sth)
+check.out_advice()
+		
